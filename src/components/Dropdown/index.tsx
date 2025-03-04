34eb31b7f263a7ca1d02e850f8ca8962d154f7e7
MEGUMMY1 <cys10902@nate.com>
2025-03-04 21:41:16 +0900

    fix: Dropdown 충돌 해결

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -4,7 +4,7 @@ import IconComponent from '../Asset/Icon';
 interface DropdownProps {
   options: string[];
   selectedOption?: string;
-  onSelect: (option: string) => void;
+  onSelect?: (option: string) => void;
   type: 'meatball' | 'select';
 }
 
@@ -14,13 +14,13 @@ export default function Dropdown({ options, selectedOption, onSelect, type }: Dr
 
   const toggleDropdown = () => setIsOpen(!isOpen);
 
-  const handleClickOutside = (event: MouseEvent) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
-      setIsOpen(false);
-    }
-  };
-
   useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    }
+
     document.addEventListener('mousedown', handleClickOutside);
     return () => {
       document.removeEventListener('mousedown', handleClickOutside);
@@ -42,7 +42,7 @@ export default function Dropdown({ options, selectedOption, onSelect, type }: Dr
           <button
             type="button"
             onClick={toggleDropdown}
-            className="flex justify-center items-center pl-4 pr-2 py-2 text-sm font-medium rounded-md border border-gray-300 text-gray-800 bg-white hover:bg-gray-100 focus:outline-none"
+            className="flex justify-center items-center pl-4 pr-2 py-2 text-r-14 rounded-md border border-gray-300 text-gray-800 bg-white hover:bg-gray-100 focus:outline-none"
           >
             {selectedOption || 'Select an option'}
             <IconComponent name={isOpen ? 'arrowUp' : 'arrowDown'} width={24} height={24} isBtn />
@@ -53,14 +53,16 @@ export default function Dropdown({ options, selectedOption, onSelect, type }: Dr
       {isOpen && (
         <div className="z-20 origin-top-right mt-1 absolute right-0 min-w-[156px] max-w-[220px] rounded-[4px] shadow-lg bg-white ring-1 ring-black ring-opacity-5">
           <div className="p-1">
-            {options.map((option) => (
+            {options.map((option, index) => (
               <button
-                key={option}
+                key={index}
                 onClick={() => {
-                  onSelect(option);
+                  if (onSelect) {
+                    onSelect(option);
+                  }
                   setIsOpen(false);
                 }}
-                className="w-full p-2 rounded-[4px] text-sm text-left text-gray-700 hover:bg-gray-100"
+                className="w-full p-2 rounded-[4px] text-r-14 text-left text-gray-700 hover:bg-gray-100"
               >
                 {option}
               </button>
