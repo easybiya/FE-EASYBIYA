6700efdbcfafa62264edfd587fbe26291ce5da8e
sj0724 <sj07245@naver.com>
2025-03-05 22:23:32 +0900

    feat: shadcn form 설치

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,56 +1,100 @@
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
+    darkMode: ['class'],
+    content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
-        violet: {
-          900: '#1E0E4B',
-          800: '#240D63',
-          700: '#311B92',
-          600: '#4527A0',
-          500: '#5E35B1',
-          400: '#7E57C2',
-          300: '#B39DDB',
-          200: '#D1C4E9',
-          100: '#EDE7F6',
-        },
-        gray: {
-          900: '#121212',
-          800: '#1F1F1F',
-          700: '#333333',
-          600: '#4F4F4F',
-          500: '#828282',
-          400: '#BDBDBD',
-          300: '#E0E0E0',
-          200: '#EEEEEE',
-          100: '#F5F5F5',
-        },
-        red: {
-          900: '#600000',
-          600: '#E53935',
-          500: '#EF5350',
-          400: '#EF9A9A',
-          100: '#FFEBEE',
-        },
-        green: {
-          900: '#003300',
-          600: '#43A047',
-          500: '#4CAF50',
-          400: '#81C784',
-          100: '#E8F5E9',
-        },
-      },
-    },
+  	extend: {
+  		colors: {
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			violet: {
+  				'100': '#EDE7F6',
+  				'200': '#D1C4E9',
+  				'300': '#B39DDB',
+  				'400': '#7E57C2',
+  				'500': '#5E35B1',
+  				'600': '#4527A0',
+  				'700': '#311B92',
+  				'800': '#240D63',
+  				'900': '#1E0E4B'
+  			},
+  			gray: {
+  				'100': '#F5F5F5',
+  				'200': '#EEEEEE',
+  				'300': '#E0E0E0',
+  				'400': '#BDBDBD',
+  				'500': '#828282',
+  				'600': '#4F4F4F',
+  				'700': '#333333',
+  				'800': '#1F1F1F',
+  				'900': '#121212'
+  			},
+  			red: {
+  				'100': '#FFEBEE',
+  				'400': '#EF9A9A',
+  				'500': '#EF5350',
+  				'600': '#E53935',
+  				'900': '#600000'
+  			},
+  			green: {
+  				'100': '#E8F5E9',
+  				'400': '#81C784',
+  				'500': '#4CAF50',
+  				'600': '#43A047',
+  				'900': '#003300'
+  			},
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
 
 export default config;
