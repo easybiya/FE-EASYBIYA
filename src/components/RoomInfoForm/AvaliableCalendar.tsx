3e506b35b50d9670be6b252748a3de2887f98428
sj0724 <sj07245@naver.com>
2025-03-11 01:15:51 +0900

    feat: 캘린더 컴포넌트 구현

---
+++ src/components/RoomInfoForm/AvaliableCalendar.tsx
@@ -0,0 +1,35 @@
+import { formatDate } from '@/utils/formatDate';
+import DatePicker from 'react-datepicker';
+import 'react-datepicker/dist/react-datepicker.css';
+
+interface Props {
+  handleCalendar: (date: string) => void;
+  currentDate: string;
+}
+
+export default function AvailableCalendar({ handleCalendar, currentDate }: Props) {
+  return (
+    <div className="relative left-0 top-full z-10 flex w-full items-center justify-center bg-white p-12 shadow-lg rounded">
+      <DatePicker
+        selected={new Date(currentDate) ? new Date(currentDate) : null}
+        onChange={(e) => handleCalendar(formatDate(e!))}
+        minDate={new Date()}
+        isClearable
+        dateFormat="yyyy-MM-dd"
+        inline
+      />
+      <style jsx>{`
+        :global(.react-datepicker__header) {
+          background-color: transparent !important;
+          border: none !important;
+        }
+
+        :global(.react-datepicker__day--selected),
+        :global(.react-datepicker__day--keyboard-selected) {
+          background-color: #23262a !important;
+          color: white !important;
+        }
+      `}</style>
+    </div>
+  );
+}
