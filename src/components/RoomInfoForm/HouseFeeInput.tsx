5cefed9d30d560b983a34b258a78fd42d772b93a
sj0724 <sj07245@naver.com>
2025-03-11 01:16:14 +0900

    fix: 금액 인풋 수정

--- src/components/RoomInfoForm/HouseFeeInput.tsx
+++ src/components/RoomInfoForm/HouseFeeInput.tsx
@@ -4,23 +4,28 @@ import Input from '../Input';
 interface Props {
   type: FeeType;
   text: string;
+  onChange: (fee: string) => void;
 }
 
-export default function HouseFeeInput({ type, text }: Props) {
+export default function HouseFeeInput({ type, text, onChange }: Props) {
   return (
-    <div className="flex gap-4 text-nowrap items-center w-full">
+    <div className="flex gap-4 text-nowrap items-center justify-between w-full">
       <p className="min-w-[41px]">{text}</p>
-      {type === 'DEPOSIT' && (
-        <div className="flex items-center gap-1">
-          <div className="w-[60px]">
-            <Input placeholder="000" />
+      <div className="flex gap-2 w-fit">
+        {type === 'DEPOSIT' && (
+          <div className="flex items-center gap-1">
+            <div className="w-[60px]">
+              <Input type="number" placeholder="000" />
+            </div>
+            <p>억</p>
           </div>
-          <p>억</p>
+        )}
+        <div className="flex items-center gap-1">
+          <div className="w-24 text-right">
+            <Input placeholder="0000" onChange={(e) => onChange(e.target.value)} />
+          </div>
+          만원
         </div>
-      )}
-      <div className="flex items-center gap-1 w-full">
-        <Input placeholder="0000" />
-        만원
       </div>
     </div>
   );
