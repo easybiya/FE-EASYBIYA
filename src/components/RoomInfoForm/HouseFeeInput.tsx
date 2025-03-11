1d918ea5bc067118f5331389f24dfab3e5955ab3
sj0724 <sj07245@naver.com>
2025-03-11 12:36:26 +0900

    fix: 억 단위 인풋 값 설정, 인풋 기본 값 설정 및 최대 값 설정

--- src/components/RoomInfoForm/HouseFeeInput.tsx
+++ src/components/RoomInfoForm/HouseFeeInput.tsx
@@ -1,28 +1,59 @@
+import { useState } from 'react';
 import { FeeType } from '@/types';
 import Input from '../Input';
 
 interface Props {
   type: FeeType;
   text: string;
-  onChange: (fee: string) => void;
+  value?: number;
+  onChange: (fee: number | undefined) => void;
 }
 
-export default function HouseFeeInput({ type, text, onChange }: Props) {
+export default function HouseFeeInput({ type, text, value = undefined, onChange }: Props) {
+  const [billion, setBillion] = useState(value ? Math.floor(value / 100_000_000) : undefined);
+  const [million, setMillion] = useState(
+    value ? Math.floor((value % 100_000_000) / 10_000) : undefined,
+  );
+
+  const handleBillionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newBillion = e.target.value ? Number(e.target.value) : undefined;
+    setBillion(newBillion);
+    onChange((newBillion ?? 0) * 100_000_000 + (million ?? 0) * 10_000);
+  };
+
+  const handleMillionChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newMillion = e.target.value ? Number(e.target.value) : undefined;
+    setMillion(newMillion);
+    onChange((billion ?? 0) * 100_000_000 + (newMillion ?? 0) * 10_000);
+  };
+
   return (
     <div className="flex gap-4 text-nowrap items-center justify-between w-full">
       <p className="min-w-[41px]">{text}</p>
-      <div className="flex gap-2 w-fit">
+      <div className="flex gap-2">
         {type === 'DEPOSIT' && (
           <div className="flex items-center gap-1">
             <div className="w-[60px]">
-              <Input type="number" placeholder="000" />
+              <Input
+                type="number"
+                placeholder="0"
+                max={999}
+                value={billion ?? ''}
+                onChange={handleBillionChange}
+              />
             </div>
             <p>억</p>
           </div>
         )}
         <div className="flex items-center gap-1">
           <div className="w-24 text-right">
-            <Input placeholder="0000" onChange={(e) => onChange(e.target.value)} />
+            <Input
+              max={9999}
+              type="number"
+              placeholder="0000"
+              value={million ?? ''}
+              onChange={handleMillionChange}
+            />
           </div>
           만원
         </div>
