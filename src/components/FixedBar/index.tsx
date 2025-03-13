5464c940b3871836baae97b8db4f5f09103a381b
sj0724 <sj07245@naver.com>
2025-03-13 10:49:52 +0900

    feat: 매물 고정 바 구현, 매물 생성 route에서는 하단 바 제외

---
+++ src/components/FixedBar/index.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import Button from '../Button/CustomButton';
+
+export default function FixedBar() {
+  return (
+    <div className="flex flex-col items-center w-full bg-primary fixed bottom-0 left-1/2 max-w-[428px] -translate-x-1/2 z-10">
+      <div className="absolute -top-10 left-0 w-full h-10 bg-gradient-to-b from-transparent to-primary" />
+      <div className="w-full p-5 flex flex-col items-center gap-2">
+        <Button label="완료" className="w-full" />
+        <Link href="/" className="py-3 text-[15px] text-gray-800 font-semibold">
+          건너 뛰기
+        </Link>
+      </div>
+    </div>
+  );
+}
