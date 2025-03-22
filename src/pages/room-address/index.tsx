c6ba06f017172ed64fa707dcfc08ef087ec2dde9
sj0724 <sj07245@naver.com>
2025-03-22 23:43:58 +0900

    style: 레이아웃 스타일 통일

--- src/pages/room-address/index.tsx
+++ src/pages/room-address/index.tsx
@@ -4,8 +4,10 @@ import SearchAddress from '@/components/map/SearchAddress';
 
 export default function RoomInfoPage() {
   return (
-    <div className="px-4 pt-6 pb-4 bg-[#F6F5F2] h-screen">
-      <HeaderWithProgress title="주소 입력" totalSteps={4} />
+    <div className="px-4 bg-[#F6F5F2] h-screen">
+      <div className="pt-10">
+        <HeaderWithProgress title="주소 입력" totalSteps={4} />
+      </div>
       <SearchAddress />
     </div>
   );
