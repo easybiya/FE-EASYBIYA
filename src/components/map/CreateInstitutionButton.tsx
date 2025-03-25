798aaf31447ffcef104c7f3dd174469c8bebebe1
sj0724 <sj07245@naver.com>
2025-03-25 22:39:52 +0900

    feat: 위치 등록 버튼 구현

--- src/components/map/CreateInstitutionButton.tsx
+++ src/components/map/CreateInstitutionButton.tsx
@@ -1,13 +1,18 @@
-import Link from 'next/link';
 import IconComponent from '../Asset/Icon';
 
-export default function CreateInstitutionButton() {
+interface Props {
+  handleClick: () => void;
+}
+
+export default function CreateInstitutionButton({ handleClick }: Props) {
   return (
-    <Link href="/map/create">
-      <div className="flex gap-1 items-center justify-center bg-white px-4 py-2 rounded-md border border-black font-semibold">
-        <IconComponent name="plus" alt="플러스 아이콘" width={12} height={12} />
-        <p className="leading-tight">직장/학교 주소 등록</p>
-      </div>
-    </Link>
+    <button
+      type="button"
+      onClick={handleClick}
+      className="flex gap-1 w-full items-center justify-center bg-white px-4 py-2 rounded-md border border-black font-semibold"
+    >
+      <IconComponent name="plus" alt="플러스 아이콘" width={12} height={12} />
+      <p className="leading-tight">직장/학교 주소 등록</p>
+    </button>
   );
 }
