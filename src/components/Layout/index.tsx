e95e194bd3c97988d6fe62617e505f7639fd6cd1
sj0724 <sj07245@naver.com>
2025-03-25 20:58:18 +0900

    fix: 지도 페이지 헤더 삭제 및 하단바 생성

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -8,8 +8,8 @@ interface LayoutProps {
 }
 
 // 숨길 페이지 목록
-const HIDDEN_TABBAR_PAGES = ['/login', '/map', '/details', '/create'];
-const HIDDEN_HEADER_PAGES = ['/login', '/details', '/create'];
+const HIDDEN_TABBAR_PAGES = ['/login', '/details', '/create'];
+const HIDDEN_HEADER_PAGES = ['/login', '/details', '/create', '/map'];
 const HIDEEN_LAYOUT_ROUTE = '/create';
 
 export default function Layout({ children }: LayoutProps) {
@@ -20,7 +20,7 @@ export default function Layout({ children }: LayoutProps) {
 
   return (
     <div className="flex justify-center w-full min-h-screen bg-white">
-      <div className="relative w-full max-w-[430px] min-h-screen bg-white shadow-[inset_1px_0_#eee,inset_-1px_0_#eee]">
+      <div className="relative w-full max-w-[430px] min-h-screen bg-primary shadow-[inset_1px_0_#eee,inset_-1px_0_#eee]">
         {/* 특정 페이지에서 Header 숨기기 */}
         {!hideHeader && <Header type={2} title="내 집 후보" />}
         {children}
