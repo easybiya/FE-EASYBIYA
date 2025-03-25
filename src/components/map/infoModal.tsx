3bbd52ea9df9a1c48dbb64bb0677b91c53b03591
sj0724 <sj07245@naver.com>
2025-03-25 20:58:48 +0900

    fix: 모달 타입 변경

--- src/components/map/infoModal.tsx
+++ src/components/map/infoModal.tsx
@@ -1,43 +1,20 @@
 import Link from 'next/link';
-import { Item } from '@/types';
+import { Institution, Item, Property } from '@/types';
 import RouteInfo from './routeInfo';
 
-declare global {
-  interface Window {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    kakao: any;
-  }
-}
-
 interface Props {
-  modalContent: Item;
+  modalContent: Property;
+  institution: Institution;
   closeModal: () => void;
 }
 
-const fixedData = [
-  {
-    id: 1,
-    name: '집',
-    address: '서울특별시 중랑구 면목로 35길 22-4',
-  },
-  {
-    id: 2,
-    name: '직장',
-    address: '서울특별시 중구 수표동 56-1',
-  },
-];
-
-export default function InfoModal({ modalContent, closeModal }: Props) {
+export default function InfoModal({ modalContent, closeModal, institution }: Props) {
   return (
-    <div className="absolute top-32 z-50">
-      <div className="relative w-[450px] flex flex-col p-5 gap-2 h-fit bg-white rounded-lg shadow-lg">
-        <p className="text-lg font-bold">{modalContent?.name}</p>
-        <p>주소: {modalContent?.address}</p>
-        {fixedData.map((item) => (
-          <div key={item.id}>
-            <RouteInfo fixedSpot={item} spotAddress={modalContent.address} />
-          </div>
-        ))}
+    <div className="absolute top-32 right-0 left-0 z-50">
+      <div className="relative flex flex-col p-5 gap-2 h-fit bg-white rounded-lg shadow-lg">
+        <p className="text-lg font-bold">{modalContent?.propertyName}</p>
+        <p>주소: {modalContent?.propertyAddress}</p>
+        <RouteInfo fixedSpot={institution} spotAddress={modalContent.propertyAddress} />
         <Link href={`/detail/${modalContent.id}`}>상세보기</Link>
         <button className="absolute top-2 right-2" onClick={closeModal}>
           닫기
