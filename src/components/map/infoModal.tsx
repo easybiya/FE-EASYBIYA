305c05b9d2b53cedccadf3c8a35a0df2ddf7773c
sj0724 <sj07245@naver.com>
2025-03-26 17:14:49 +0900

    feat: 경로 표시 컴포넌트 구현 및 적용

--- src/components/map/infoModal.tsx
+++ src/components/map/infoModal.tsx
@@ -1,6 +1,8 @@
-import Link from 'next/link';
 import { Institution, Property } from '@/types';
 import RouteInfo from './routeInfo';
+import IconComponent from '../Asset/Icon';
+import { useState } from 'react';
+import DetailRouteModal from './DetailRouteModal';
 
 interface Props {
   modalContent: Property;
@@ -9,17 +11,53 @@ interface Props {
 }
 
 export default function InfoModal({ modalContent, closeModal, institution }: Props) {
+  const [isDetail, setIsDetail] = useState(false);
+  const isInstitution = modalContent.propertyAddress === institution.institutionAddress;
+
   return (
-    <div className="absolute top-32 right-0 left-0 z-50">
-      <div className="relative flex flex-col p-5 gap-2 h-fit bg-white rounded-lg shadow-lg">
-        <p className="text-lg font-bold">{modalContent?.propertyName}</p>
-        <p>주소: {modalContent?.propertyAddress}</p>
-        <RouteInfo fixedSpot={institution} spotAddress={modalContent.propertyAddress} />
-        <Link href={`/detail/${modalContent.id}`}>상세보기</Link>
-        <button className="absolute top-2 right-2" onClick={closeModal}>
-          닫기
-        </button>
+    <>
+      <div className="absolute top-5 right-0 left-0 z-50 m-5">
+        <div className="relative flex flex-col items-start p-5 gap-2 h-fit bg-white rounded-xl shadow">
+          <IconComponent
+            name={`${isInstitution ? 'institution' : 'property'}`}
+            width={28}
+            height={28}
+            alt="모달 아이콘"
+          />
+          <p className="text-md font-bold">{modalContent?.propertyName}</p>
+          <p className="text-[15px]">{modalContent?.propertyAddress}</p>
+          <div
+            className="w-fit flex items-center gap-[2px] rounded-full border px-4 py-2.5 font-semibold cursor-pointer"
+            onClick={() => setIsDetail(true)}
+          >
+            <p className="h-5 leading-tight text-sm">대중교통 보기</p>
+            <div className="h-[14px] w-[14px]">
+              <IconComponent
+                name="arrowRight"
+                width={10}
+                height={10}
+                alt="오른쪽 화살표"
+                className="cursor-pointer"
+              />
+            </div>
+          </div>
+          <IconComponent
+            name="close"
+            alt="닫기 아이콘"
+            width={16}
+            height={16}
+            onClick={closeModal}
+            className="absolute top-5 right-5 cursor-pointer"
+          />
+        </div>
       </div>
-    </div>
+      {isDetail && !isInstitution && (
+        <DetailRouteModal
+          institution={institution}
+          currentAddress={modalContent}
+          isClose={() => setIsDetail(false)}
+        />
+      )}
+    </>
   );
 }
