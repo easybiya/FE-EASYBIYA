e52cd67ceeb3a61add2eb0aec75f3d8cee9d1c44
sj0724 <sj07245@naver.com>
2025-03-28 09:43:02 +0900

    feat: 기본 정보 페이지 구현

--- src/pages/profile/info/index.tsx
+++ src/pages/profile/info/index.tsx
@@ -1,3 +1,19 @@
+import IconComponent from '@/components/Asset/Icon';
+import Header from '@/components/Layout/Header';
+
 export default function Page() {
-  return <div>기본 정보</div>;
+  return (
+    <div>
+      <Header title="기본 정보" type={4} />
+      <div className="flex flex-col gap-3 px-5 py-2.5">
+        <div className="flex items-center gap-2.5 py-3">
+          <span className="w-5 h-5 bg-[#FEE500] flex items-center justify-center rounded-sm">
+            <IconComponent name="kakao" width={12} height={12} />
+          </span>
+          <p className="font-semibold text-base leading-tight">000000@gmail.com</p>
+        </div>
+        <div className="font-semibold text-base leading-tight py-3">회원 탈퇴</div>
+      </div>
+    </div>
+  );
 }
