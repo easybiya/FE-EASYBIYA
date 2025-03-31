7fdff33e3bfc8d9aa4896d38c63a070f5b2bcb07
sj0724 <sj07245@naver.com>
2025-03-31 14:51:14 +0900

    style: 하단 스타일 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -55,7 +55,7 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="flex flex-col px-5 py-2 gap-2">
+    <div className="flex flex-col px-5 py-2 gap-2 mb-20">
       <div className="flex w-full justify-between items-center">
         <p className="text-gray-500 text-[14px]">전체 {mockHouserData.length}</p>
         <Dropdown options={DROPDOWN_OPTION} type="select" selectedOption="최신순" />
