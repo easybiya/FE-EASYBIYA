6ec9a2280b2892ca0260dd22f9c20c6f26727a42
sj0724 <sj07245@naver.com>
2025-04-13 01:37:16 +0900

    fix: 날짜 포멧 로직 종류 수정

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,6 +1,13 @@
 import { format } from 'date-fns';
+import { ko } from 'date-fns/locale';
 
-export const formatDate = (date: Date) => {
-  const formattedDate = format(date, 'yyyy-MM-dd');
-  return formattedDate;
+export const formatDate = (date: Date, type: 1 | 2) => {
+  if (type === 1) {
+    const formattedDate = format(date, 'yyyy-MM-dd');
+    return formattedDate;
+  }
+  if (type === 2) {
+    const formattedDate = format(date, 'yyyy. MM. dd. EEE', { locale: ko });
+    return formattedDate;
+  }
 };
