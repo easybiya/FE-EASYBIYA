787ee636628bfc5b655a73bef679a71ea2368350
sj0724 <sj07245@naver.com>
2025-04-13 01:37:41 +0900

    add: 체크 리스트 mock data 추가

--- src/data/mockHouseData.ts
+++ src/data/mockHouseData.ts
@@ -1,5 +1,5 @@
 import { Institution, Property } from '@/types';
-import { ChecklistTemplate } from '@/types/checklist';
+import { CheckList, ChecklistTemplate } from '@/types/checklist';
 
 export const mockHouserData: Property[] = [
   {
@@ -96,7 +96,7 @@ export const mockInstitutionData: Institution = {
   institutionLongitude: 126.9784155,
 };
 
-export const mockCheckList: ChecklistTemplate = {
+export const mockCheckListTemplate: ChecklistTemplate = {
   name: '기본 템플릿',
   checklists: [
     {
@@ -192,3 +192,56 @@ export const mockCheckList: ChecklistTemplate = {
     },
   ],
 };
+
+export const mockCheckList: CheckList = [
+  {
+    title: '집 주인 전화번호',
+    checkType: 'TEXT',
+    content: '010-1234-5467',
+    priority: 1,
+    checkItems: [],
+  },
+  {
+    title: '방 내부 점검',
+    checkType: 'TEXT',
+    content: '벽지, 바닥, 창문 상태 확인',
+    priority: 2,
+    checkItems: [],
+  },
+  {
+    title: '주변 환경 점검',
+    checkType: 'CHECKBOX',
+    content: '',
+    priority: 3,
+    checkItems: [
+      {
+        description: '편의점 접근성',
+        checked: false,
+        priority: 1,
+      },
+      {
+        description: '대중교통 접근성',
+        checked: true,
+        priority: 2,
+      },
+    ],
+  },
+  {
+    title: '설비 상태 체크',
+    checkType: 'RADIO',
+    content: '',
+    priority: 4,
+    checkItems: [
+      {
+        description: '가스레인지 정상 작동',
+        checked: true,
+        priority: 1,
+      },
+      {
+        description: '전등 정상 작동',
+        checked: false,
+        priority: 2,
+      },
+    ],
+  },
+];
