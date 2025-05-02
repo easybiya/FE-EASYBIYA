cb03a7c28e8acaf56c60b9913a9fb1a3294b5d29
eqypo9 <eqypo@naver.com>
2025-05-03 01:28:54 +0900

    feat: 특정 매물의 이미지 수정 put 요청 추가

--- src/lib/api/property.ts
+++ src/lib/api/property.ts
@@ -1,3 +1,5 @@
+import instance from "./axiosInstance";
+
 export const postProperty = async (formData: FormData) => {
   const res = await fetch('http://localhost:8080/api/property', {
     method: 'POST',
@@ -9,4 +11,15 @@ export const postProperty = async (formData: FormData) => {
   }
 
   return await res.json();
+};
+
+export const updatePropertyImages = async (propertyId: number, formData: FormData) => {
+  const res = await instance.put(`/api/property/${propertyId}/images`, formData, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  });
+
+  if (!res.data) throw new Error('이미지 수정 실패');
+  return res.data;
 };
