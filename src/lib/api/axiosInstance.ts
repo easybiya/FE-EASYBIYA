ea135e0524f281e30fc4748137f5ffb59a893d03
sj0724 <sj07245@naver.com>
2025-05-18 19:43:30 +0900

    Fix: 로그인 후 토큰 쿠키 설정

--- src/lib/api/axiosInstance.ts
+++ src/lib/api/axiosInstance.ts
@@ -1,11 +1,24 @@
 import axios from 'axios';
+import { Cookies } from 'react-cookie';
 
-// TO DO
-// 도메인 나오면 수정
+const cookies = new Cookies();
 
 const instance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080/api',
   withCredentials: true,
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const token = cookies.get('accessToken');
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
 
 export default instance;
