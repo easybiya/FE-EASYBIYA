a9a8581a48972e56dae6e443bd31045d5c33add0
sj0724 <sj07245@naver.com>
2025-07-05 11:54:50 +0900

    Add: 미들웨어 설정

---
+++ src/middleware.ts
@@ -0,0 +1,19 @@
+import { NextResponse, NextRequest } from 'next/server';
+
+export function middleware(request: NextRequest) {
+  const { cookies } = request;
+
+  const authToken = cookies.get('accessToken')?.value;
+
+  if (!authToken) {
+    // 인증 토큰 없으면 리디렉트
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  // 인증 토큰 있으면 요청 계속 진행
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ['/((?!login|api|_next|onboarding|oauth2|share|icons|images).*)'],
+};
