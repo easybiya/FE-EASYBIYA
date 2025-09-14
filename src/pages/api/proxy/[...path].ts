829920d826b07d9137604559202bfda4556ec6b8
sj0724 <sj07245@naver.com>
2025-09-14 20:18:15 +0900

    Fix: 타입 에러 수정

--- src/pages/api/proxy/[...path].ts
+++ src/pages/api/proxy/[...path].ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
